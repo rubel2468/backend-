ebc3ddfe63be414fc65bfd39fa7a8d225b35b240
Rubel <132301506+rubel2468@users.noreply.github.com>
2024-12-15 19:46:21 +0600

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://backend-ecommarce-22.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     maxAge:3600,
